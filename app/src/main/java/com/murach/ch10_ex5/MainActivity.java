0d22ec844762ac0d521833a2ec082319994190ef
660252222 <ivanesparza1991@gmail.com>
2016-09-21 09:27:51 -0700

    Initial commit

---
+++ app/src/main/java/com/murach/ch10_ex5/MainActivity.java
@@ -0,0 +1,73 @@
+package com.murach.ch10_ex5;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    private TextView messageTextView;
+    private Button startButton;
+    private Button stopButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        messageTextView = (TextView) findViewById(R.id.messageTextView);
+        startTimer();
+    }
+
+    private void startTimer() {
+        final long startMillis = System.currentTimeMillis();
+        Timer timer = new Timer(true);
+        TimerTask task = new TimerTask() {
+
+            @Override
+            public void run() {
+                long elapsedMillis = System.currentTimeMillis() - startMillis;
+                updateView(elapsedMillis);
+            }
+        };
+        timer.schedule(task, 0, 1000);
+    }
+
+    private void updateView(final long elapsedMillis) {
+        // UI changes need to be run on the UI thread
+        messageTextView.post(new Runnable() {
+
+            int elapsedSeconds = (int) elapsedMillis/1000;
+
+            @Override
+            public void run() {
+                messageTextView.setText("Seconds: " + elapsedSeconds);
+            }
+        });
+    }
+
+    public void onPause()
+    {
+        super.onPause();
+        if(messageTextView != null)
+        {
+            messageTextView.cancelLongPress();
+            messageTextView = null;
+        }
+    }
+
+
+    public void setStopButton()
+    {
+        stopButton = (Button) findViewById(R.id.stopButton);
+    }
+
+    public void setStartButton()
+    {
+        startButton = (Button) findViewById(R.id.startButton);
+    }
+}
